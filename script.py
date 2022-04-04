4db49eec3cf7d984401661a302d222bd741bf23a
Josh Ajiniran <josuajiniran@gmail.com>
2022-04-04 13:26:06 +0100

    naive get total_distance

--- script.py
+++ script.py
@@ -12,13 +12,12 @@ except IndexError:
     exit()
 
 gpd.io.file.fiona.drvsupport.supported_drivers["KML"] = "rw"
-my_map = gpd.read_file(kml_file, driver="KML")
-my_map.to_crs(epsg=32663, inplace=True)
+map_data = gpd.read_file(kml_file, driver="KML")
 
 lats = []
 lons = []
 
-for feature in my_map.geometry:
+for feature in map_data.geometry:
     if isinstance(feature, LineString):
         linestrings = [feature]
     elif isinstance(feature, MultiLineString):
@@ -31,4 +30,9 @@ for feature in my_map.geometry:
         lats = np.append(lats, y)
         lons = np.append(lons, x)
 
-geos = [Point(xy) for xy in zip(lons, lats)]
+geom = [Point(xy) for xy in zip(lons, lats)]
+gdf = gpd.GeoDataFrame(geometry=geom, crs={'init': 'epsg:4326'})
+
+gdf.to_crs(epsg=32663, inplace=True)
+total_distance = gdf.distance(gdf.shift())
+print(f'{total_distance/1000}km')
